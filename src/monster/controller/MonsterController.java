ad33885234b0c0e428390a19030da6d6c485e765
talm8994 <tom.tom.pop13@gmail.com>
2015-09-15 13:51:47 -0600

    its all fixed

--- src/monster/controller/MonsterController.java
+++ src/monster/controller/MonsterController.java
@@ -2,13 +2,14 @@ package monster.controller;
 
 import monster.model.Monster;
 import marshmallows.view.monsterDisplay;
+import java.util.Scanner;
 public class MonsterController
 {
 	private Monster thomasMonster;
 	private monsterDisplay myDisplay;
+	private Scanner monsterScanner;
 	
 	public MonsterController()
-	
 	{
 		String name = "beacky";
 		int eyes = 2;
@@ -17,11 +18,21 @@ public class MonsterController
 		double hair = 1.1;
 		boolean hasBellyButton = true;
 		
+		monsterScanner = new Scanner(System.in);
 		myDisplay = new monsterDisplay();
 		thomasMonster = new Monster(name, eyes, noses, legs, hair, hasBellyButton);
 	}
 	public void start()
 	{
 		myDisplay.displayInfo(thomasMonster.toString());
+		askQuestions();
+		myDisplay.displayInfo("Updated monster info: " + thomasMonster.toString());
+	}
+	
+	private void askQuestions()
+	{
+		System.out.println("I want a new name for a monster, type one please!!");
+		String newMonsterName = monsterScanner.next();
+		thomasMonster.setmonsterName(newMonsterName);
 	}
 }
